7932cdc239da616c86a1f20194af8f400ac8f2cd
Teran326 <swysog@gmail.com>
2020-04-23 16:46:32 +0200

    zmena v selectors.js, prepsani body na this

--- js/selectors.js
+++ js/selectors.js
@@ -4,7 +4,7 @@ $(function(){
             $(this).rvReverseText().show(300, "swing", function(){
                 let img = "img/death.jpg";
                 $(this).empty().css('background-image', `url(${img})`).delay(1000).queue(function(next) { $(this).attr('style', `background-color: black`); next(); });
-                $('body').append('<h1></h1>').attr('class', 'text-center');
+                $(this).append('<h1></h1>').attr('class', 'text-center');
                 $('h1').text("KABOOM").hide(0).attr('class', 'text-white').delay(1000).show(3000);
             });
         });
