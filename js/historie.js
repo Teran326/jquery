effd33f7fe116da1a03047400f3e86be1579d4a3
Teran326 <swysog@gmail.com>
2020-05-12 11:18:08 +0200

    historie Polska

---
+++ js/historie.js
@@ -0,0 +1,156 @@
+const events = [
+    {
+       "year":"9. století",
+       "event":"Vznik polska jako státu",
+       "detail":"S počátky státní organizace se na polském území setkáváme již ve druhé polovině 9. století. Zárodky státu vznikly současně ve dvou centrech. Byla to knížectví Vislanů na jihu země a knížectví Polanů na severozápadě. Nezávisle na sobě se vyvíjely až do konce 10. století, kdy bylo území Vislanů – Krakovsko připojeno ke knížectví Polanů.",
+       "evaluation":"true"
+    },
+    {
+        "year":"1289",
+        "event":"Přemyslovci v Polsku",
+        "detail":"Český král Václav II. od roku 1289 začal rozšiřovat svoji lenní svrchovanost nad jednotlivými slezskými knížectvími, roku 1291 ovládl díky smrti svého bratrance Jindřicha IV. Krakovsko. Jeho soupeř, Piastovec Vladislav Lokýtek, který se rovněž pokoušel sjednotit polská knížectví pod svojí vládou, byl nucen uprchnout ze země. Roku 1300 se ovdovělý Václav oženil s dvanáctiletou princeznou Eliškou Rejčkou, jejímuž otci Přemyslu II. se nedlouho před smrtí podařilo obnovit v Polsku království.",
+        "evaluation":"true"
+    },
+    {
+       "year":"1795",
+       "event":"Rozdělení polska",
+       "detail":"Posilování šlechty a vnitřní rozbroje během 17. století oslabily zemi, takže v letech 1772, 1793 a 1795 si Rusko, Prusko a Rakousko v trojím dělení rozdělily Polsko na základě vzájemné dohody. Po třetím dělení Polska v roce 1795 tak Rzeczpospolita zanikla.",
+       "evaluation":"false"
+    },
+    {
+        "year":"1807",
+        "event":"Znovuzřízení polska",
+        "detail":"V letech 1807–1815 zřídil francouzský císař Napoleon I. na části území předtím zabraným Pruskem a Rakouskem Varšavské knížectví, které se stalo spojencem Napoleona a jehož knížetem byl Fridrich August I. Saský, vnuk předposledního polského krále. Po jeho porážce bylo zrušeno a na jeho místě bylo zřízeno autonomní Polské království také nazývané „Kongresovka“. Tento stát podléhal Rusku, jehož panovníkem byl také ruský imperátor. Polsko tak bylo spojené personální unií s Ruskem.",
+        "evaluation":"true"
+    },
+    {
+        "year":"1939",
+        "event":"2. světová válka",
+        "detail":"Dne 1. září 1939 zahájili Němci útokem na Polsko druhou světovou válku. 17. září 1939 vtrhla do východní části Polska sovětská vojska a obsadila území, která náležela od konce 18. století do roku 1920 Rusku. Po kapitulaci polské armády byl nařízením Hitlera zřízen na části Polska tzv. Generální gouvernement, zbylé části (Pomoří, Velkopolsko, Slezsko, Kujavsko, západní Mazovsko a části vojvodství Lodžského, Krakovského a Kieleckého) byly přičleněny přímo k Třetí říši. Vůči Polákům byl od samého počátku uplatňován krutý teror, na kterém se podílely jednotky SS-Totenkopfverbände a Einsatzgruppen. Docházelo k zatýkání a hromadným popravám polského obyvatelstva, na vesnicích byli zabíjeni rolníci, byla vyvražděna prakticky celá polská národní elita, polští intelektuálové.",
+        "evaluation":"false"
+    },
+ ];
+
+const heroes = [
+    {
+       "name":"Andrzej Sapkowski",
+       "birth":"21.6.1948",
+       "death":"ještě žije",
+       "biography":" Polský spisovatel, který je považován za jednoho z nejlepších autorů fantasy. Je velice populární nejen v Polsku, ale také v České republice, Slovensku, Rusku, Německu, v Portugalsku, Francii, Švýcarsku a obrovskou popularitu si vydobyl ve Španělsku. Jeho nejznámějším dílem jsou povídky a navazující sága o zaklínači Geraltovi z Rivie a princezně Ciri. Tato část jeho díla byla i zfilmována a byly vytvořeny hry. Od filmu a starého seriálu se však sám Sapkowski důrazně distancoval.",
+       "portraits":[
+          "andrzej1.jpg",
+       ],
+       "online":"https://cs.wikipedia.org/wiki/Andrzej_Sapkowski"
+    },
+    {
+        "name":"Fryderyk Chopin",
+        "birth":"1.3.1810",
+        "death":"17.10.1847",
+        "biography":"Jeho otec byl francouzský emigrant, matka pocházela ze zchudlého šlechtického rodu z Polska. Fryderyk Chopin prožil dětství ve Varšavě, kde se v roce 1816 začal učit hru na klavír u Čecha Vojtěcha Živného. Již v sedmi letech se pokoušel o své první skladby. Od roku 1822 byl žákem varšavského lycea a mezi léty 1826–1829 se učil u Józefa Elsnera hudební teorii na konzervatoři. Po škole slavil úspěch koncertováním ve Vídni a Drážďanech, na území dnešního Česka měl malý soukromý recitál v Teplicích. V roce 1830 opustil Polsko a vydal se do Vídně. V roce 1831 přesídlil do Paříže, kde působil také jako učitel hudby. Zde navázal vztah se spisovatelkou George Sandovou, se kterou se však nikdy neoženil, a po rozchodu s ní již nenavázal další vztah. V letech 1838–1839 pobýval na Mallorce, kde těžce onemocněl tuberkulózou. Často pobýval v lázních po celé Evropě, v roce 1848 cestoval i po Anglii a Skotsku. Své nemoci podlehl 17. října 1849 v Paříži, kde je též pohřben, jeho srdce ve stříbrné schránce je však uloženo ve Varšavě. Na jeho počest se ve Varšavě konají mezinárodní soutěže mladých klavíristů.",
+        "portraits":[
+           "fryderyk1.jpg"
+        ],
+        "online":"https://cs.wikipedia.org/wiki/Fryderyk_Chopin"
+     },
+     {
+        "name":"Jan Paweł II.",
+        "birth":"18.5.1920",
+        "death":"2.4.2005",
+        "biography":"Vlastním jménem Karol Józef Wojtyła byl polský katolický duchovní, jenž se postupně stal pomocným biskupem (1958–1963), arcibiskupem krakovským (1964–1978), kardinálem (1967) a od 16. října 1978 až do své smrti prvním slovanským a po 455 letech prvním neitalským papežem.",
+        "portraits":[
+           "jan1.jpg"
+        ],
+        "online":"https://cs.wikipedia.org/wiki/Jan_Pavel_II."
+     },
+     {
+        "name":"Kazimierz Pułaski",
+        "birth":"4.3.1745",
+        "death":"11.10.1779",
+        "biography":"Šlechtická rodina Pułaských měla pozemky ve Winiarách nedaleko Warki v Mazovském vojvodství. Právě tam Kazimierz prožil dětství. Střední školu studoval ve Varšavě. V 17 letech se stal pážetem polského prince a knížete kuronského a zemgalského Karla Kristiána. V jeho vojsku se pak roku 1763 zúčastnil bojů s Rusy o Jelgavu. V roce 1768, ve 22 letech, patřil k zakladatelům a klíčovým osobnostem tzv. barské konfederace, tedy vojenského spolku polské šlechty, který chtěl bojovat s rostoucím ruským vlivem v Polsku a s reformami krále Stanislava II. Augusta Poniatowského, jenž byl podle šlechtické opozice stále více závislý na ruské podpoře. Patřil k nejúspěšnějším velitelům vojsk barské konfederace, několik týdnů se bránil ruské přesile u berdičevského kláštera (na území dnešní Ukrajiny), v bitvě pod Orzechowem se pak utkal s ruským vojskem vedeným slavným Alexandrem Suvorovem. V boji ztratil bratra Franciszka Ksaweryho, sám byl dvakrát raněn. Slavnou se stala rovněž jeho obrana kláštera v Čenstochové, z Pulaskiho se tak stal nový symbol národního odporu. Celé povstání nakonec zkrachovalo, když Polsko ztratilo podporu Rakouska i Pruska, které cítily šanci ukousnout si také z polského území.",
+        "portraits":[
+           "pulaski1.jpg"
+        ],
+        "online":"https://cs.wikipedia.org/wiki/Casimir_Pulaski"
+     },
+     {
+         "name":"Maria Curie - Skłodowska",
+         "birth":"7.11.1867",
+         "death":"4.7.1934",
+         "biography":"Maria Salomea Skłodowska byla nejmladší z pěti dětí učitelského páru Władysława a Bronisławy Skłodowských. Oba rodiče pocházeli z nižší polské šlechty (polsky szlachta). Otec studoval na univerzitě v Petrohradě a poté byl učitelem matematiky a fyziky. Matka byla žákyní jediné soukromé dívčí školy ve Varšavě. Posléze se stala učitelkou a ředitelkou té dívčí školy, na které studovala. Marie Skłodowska složila (pod tímto jménem) v roce 1891 jako první žena v historii přijímací zkoušky na fakultu fyziky a chemie pařížské Sorbonny. Přes den studovala a po večerech doučovala a vydělávala si tak na živobytí. V roce 1893 získala licenciát a začala pracovat jako laborantka v průmyslové laboratoři Lippmanových závodů. Současně dále studovala na Sorbonně a druhý licenciát z matematiky získala v roce 1894. Rovněž v roce 1894 poznala svého budoucího manžela Pierra Curie, který byl v té době doktorandem v laboratoři Henri Becquerela. Po Pierrově doktorátu se v roce 1895 vzali. Pierre byl bez vyznání a Maria byla nábožensky vlažná, proto měli občanský sňatek, což na nějaký čas vedlo k roztržce Marie s její polskou katolickou rodinou. Roku 1897 se jim narodila dcera Irène (taktéž pozdější nositelka Nobelovy ceny).",
+         "portraits":[
+            "maria1.jpg",
+         ],
+         "online":"https://cs.wikipedia.org/wiki/Marie_Curie-Sk%C5%82odowsk%C3%A1"
+      },
+      {
+         "name":"Robert Kubica",
+         "birth":"7.12.1984",
+         "death":"ještě žije",
+         "biography":"První polský pilot Formule 1. Kariéru přerušil poté, co se zranil při rally v Itálii, Ronde di Andora. V sezóně 2019 byl závodním jezdcem týmu Williams. Po této sezóně ukončil kariéru ve Formuli 1.",
+         "portraits":[
+            "robert1.jpg"
+         ],
+         "online":"https://cs.wikipedia.org/wiki/Robert_Kubica"
+      },
+      {
+        "name":"Wisława Szymborska",
+        "birth":"2.7.1923",
+        "death":"1.2.2012",
+        "biography":"Byla polská básnířka, esejistka, literární kritička a překladatelka francouzské literatury, laureátka Nobelovy ceny za literaturu za rok 1996. Szymborskou do češtiny překládá zejména Vlasta Dvořáčková, ale i jiní překladatelé, např. Bogdan Trojak.",
+        "portraits":[
+           "jmeno1.jpg"
+        ],
+        "online":"https://cs.wikipedia.org/wiki/Wis%C5%82awa_Szymborsk%C3%A1"
+     },
+ ];
+
+$(function(){
+    $("h2").on("click", function(){
+        $(this).parents(".row").next().toggle(1000);
+    });
+    events.forEach((event)=>{
+        $("#udalosti tbody").append(`<tr>
+            <td class="event-year">${event.year}</td>
+            <td>
+              <p class="event-name"><i class="fas fa-chevron-down"></i><b>${event.event}</b></p>
+              <p class="event-detail">${event.detail}</p>
+            </td>            
+            <td class="event-evaluation">${(event.evaluation == "true") ? '<i class="fas fa-plus-circle text-success"></i>' : '<i class="fas fa-minus-circle text-danger"></i>'}</td>
+        </tr>`);
+    });
+
+    $(".event-detail").hide();
+    $(".event-name i, .event-name b").on("click", function(){
+        $("#udalosti tr").removeClass("bg-secondary text-white");
+        $(this).parents("tr").addClass("bg-secondary text-white");  
+        $(this).parent().next().toggle(700);
+    });    
+
+    heroes.forEach((hero)=>{
+        $("#postavy .list-group").append(`<li class="list-group-item list-group-item-action list-group-item-success">${hero.name}</li>`);
+    });
+
+    function fillPersonCard(person) {
+        let hero = heroes.find(item => {return item.name === person});
+        $(".card-header").html(`<i class="fas fa-star-of-life"></i> <b>${hero.birth}</b> - <i class="fas fa-cross"></i> <b>${hero.death}</b>`);
+        $(".card-title").text(hero.name);
+        $(".card-text").text(hero.biography);
+        $(".card-footer").html(`Odkaz: <a href="${hero.online}">${hero.online}</a>`);
+        $(".gallery").empty();
+        for (let i = 0; i < hero.portraits.length; i++) {
+            $(".gallery").append(`<div class="col-sm-4"><a href="#"><img src="images/${hero.portraits[i]}" alt="" class="img-fluid"></a></div>`);        
+        }
+    }
+
+    $("#postavy li:first").addClass('active');
+    fillPersonCard(heroes[0].name);
+    $("#postavy li").on("click", function(){
+        $("#postavy li").removeClass("active");
+        $(this).addClass("active");        
+        let person = $(this).text();
+        $("#portret").slideUp(1000, function(){
+            fillPersonCard(person);
+        });
+        $("#portret").slideDown(1500);
+    });
+})
